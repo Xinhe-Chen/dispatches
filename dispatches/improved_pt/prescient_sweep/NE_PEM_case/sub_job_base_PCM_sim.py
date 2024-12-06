14edcfd6be803c273234dc139eeaba8ede308105
Xinhe Chen <xchen24@nd.edu>
2024-12-06 14:45:08 -0500

    add base PCM simulation script

---
+++ dispatches/improved_pt/prescient_sweep/NE_PEM_case/sub_job_base_PCM_sim.py
@@ -0,0 +1,30 @@
+import os
+
+this_file_path = os.path.dirname(os.path.realpath(__file__))
+
+def submit_job():
+    # create a directory to save job scripts
+    job_scripts_dir = os.path.join(this_file_path, "sim_job_scripts")
+    if not os.path.isdir(job_scripts_dir):
+        os.mkdir(job_scripts_dir)
+
+    file_name = os.path.join(job_scripts_dir, f"base_PCM_sim.sh")
+    with open(file_name, "w") as f:
+        f.write(
+            "#!/bin/bash\n"
+            + "#$ -M xchen24@nd.edu\n"
+            + "#$ -m ae\n"
+            + "#$ -q long\n"
+            + f"#$ -N base_PCM_sim\n"
+            + "conda activate regen\n"
+            + "export LD_LIBRARY_PATH=~/.conda/envs/regen/lib:$LD_LIBRARY_PATH \n"
+            + "module load gurobi/9.5.1\n"
+            + "module load ipopt/3.14.2 \n"
+            + f"python ./base_pcm_simulation.py"
+        )
+
+    os.system(f"qsub {file_name}")
+
+if __name__ == "__main__":
+    
+    submit_job()
