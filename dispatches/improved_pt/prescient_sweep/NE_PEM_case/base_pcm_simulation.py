71d38c02d695b2e95ad564de1594930a07b6fc35
Xinhe Chen <xchen24@nd.edu>
2025-06-24 11:32:40 -0400

    run base case in a new environment

--- dispatches/improved_pt/prescient_sweep/NE_PEM_case/base_pcm_simulation.py
+++ dispatches/improved_pt/prescient_sweep/NE_PEM_case/base_pcm_simulation.py
@@ -1,5 +1,5 @@
 from dispatches.improved_pt.prescient_sweep.base_prescient_options import prescient_options
 from prescient.simulator import Prescient
 
-prescient_options["output_directory"] = f"base_pcm_simulation"
+prescient_options["output_directory"] = f"base_pcm_simulation_new_env"
 Prescient().simulate(**prescient_options)
--- dispatches/improved_pt/prescient_sweep/NE_PEM_case/sub_job_base_PCM_sim.py
+++ dispatches/improved_pt/prescient_sweep/NE_PEM_case/sub_job_base_PCM_sim.py
@@ -9,7 +9,8 @@ def submit_job():
         os.mkdir(job_scripts_dir)
 
     file_name = os.path.join(job_scripts_dir, f"base_PCM_sim.sh")
-    env_path = os.path.join("..", "..", "..", "..", "..", "..", "..", ".conda", "envs", "regen")
+    # env_path = os.path.join("..", "..", "..", "..", "..", "..", "..", ".conda", "envs", "regen")
+    env_path = "prescient"
     with open(file_name, "w") as f:
         f.write(
             "#!/bin/bash\n"
