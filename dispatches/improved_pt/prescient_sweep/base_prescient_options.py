09b2f4ae2d9e0f58a30af28873aeb9b6e69a7ba6
Xinhe Chen <xchen24@nd.edu>
2025-08-05 12:52:05 -0400

    prepare PCM sweep

--- dispatches/improved_pt/prescient_sweep/base_prescient_options.py
+++ dispatches/improved_pt/prescient_sweep/base_prescient_options.py
@@ -10,7 +10,8 @@ shortfall = 500
 output_path = os.path.join("NE_PEM_case", f"base_case_pcm_benchmark")
 prescient_options = {
         "data_path":rtsgmlc_path,
-        "reserve_factor":0.0,
+        "reserve_factor": None,
+        "shortfall": shortfall,
         "simulate_out_of_sample":True,
         "output_directory":output_path,
         "monitor_all_contingencies":False,
