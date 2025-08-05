e8acd8a4cbdef1d7a6c6f11f869d22668032dea5
Xinhe Chen <xchen24@nd.edu>
2025-08-05 13:18:06 -0400

    remove unnecessary info

--- dispatches/improved_pt/prescient_sweep/base_prescient_options.py
+++ dispatches/improved_pt/prescient_sweep/base_prescient_options.py
@@ -1,6 +1,6 @@
 import os
 from dispatches_sample_data import rts_gmlc
-from dispatches.case_studies.renewables_case.double_loop_utils import read_rts_gmlc_wind_inputs
+# from dispatches.case_studies.renewables_case.double_loop_utils import read_rts_gmlc_wind_inputs
 
 
 rtsgmlc_path = rts_gmlc.source_data_path
@@ -46,8 +46,8 @@ prescient_options = {
 
 # read the wind capacity factor dataframe and make a NPP capacity factor in the same format
 # this is for the perfect forecaster
-wind_generator = "303_WIND_1"
-NPP_df = read_rts_gmlc_wind_inputs(rts_gmlc.source_data_path, wind_generator)
-NPP_df.columns = ["121_NUCLEAR_1-RTCF", "121_NUCLEAR_1-DACF"]
-NPP_df["121_NUCLEAR_1-RTCF"] = 1.0
-NPP_df["121_NUCLEAR_1-DACF"] = 1.0
+# wind_generator = "303_WIND_1"
+# NPP_df = read_rts_gmlc_wind_inputs(rts_gmlc.source_data_path, wind_generator)
+# NPP_df.columns = ["121_NUCLEAR_1-RTCF", "121_NUCLEAR_1-DACF"]
+# NPP_df["121_NUCLEAR_1-RTCF"] = 1.0
+# NPP_df["121_NUCLEAR_1-DACF"] = 1.0
